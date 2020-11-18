9d2a99a815bf3192ee9687d873e0173a3cf99f90
Chris <chrisfyi@hotmail.com>
2020-11-17 16:21:13 -0800

    New Build

---
+++ build/precache-manifest.5c32c06cbaf3bed3f3fc6c2b520f6dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17237a62b9c249d782cb078a1d324541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d35f50fb564c977c6c",
+    "url": "/static/js/2.a11093a0.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.a11093a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b55dc6f66af722cd9f5",
+    "url": "/static/js/main.e7d0689f.chunk.js"
+  },
+  {
+    "revision": "e3695a1b1352287902db",
+    "url": "/static/js/runtime-main.894ae985.js"
+  },
+  {
+    "revision": "454cbfe11f2aecac34fcdfa10c98124c",
+    "url": "/static/media/spinner.454cbfe1.gif"
+  }
+]);
